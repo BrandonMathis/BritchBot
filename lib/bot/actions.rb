0050388db5b26c6d6602c2f2d69aaf4d6c986c11
adam.rawlings <adam.rawlings@bronto.com>
2013-02-18 17:29:45 -0500

    Named the actions

--- lib/bot/actions.rb
+++ lib/bot/actions.rb
@@ -1,8 +1,20 @@
 module Bot
   module Actions
     # TODO Need to actually get some rand mfw images
+    def self.jpgTo
+
+    end
+
     def self.mfw
       GroupMe.image_message("http://i.imgur.com/qSWNG8V.png")
     end
+
+    def self.rage
+
+    end
+
+   def self.leaderBoard
+
+   end
   end
 end
